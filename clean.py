6349cc9f417921abc4f9047138999b6c0b15e650
Sofia Rest <srest2021@gmail.com>
2025-06-13 18:31:26 -0400

    colnames

---
+++ clean.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from abc import ABC, abstractmethod
+from typing import Callable, Dict, Any, List, Optional, Self, Set, Tuple, Type
+
+from utils import CustomLogger, combine_flags
--- lightcurve.py
+++ lightcurve.py
@@ -5,8 +5,8 @@ from typing import List, Optional
 
 import numpy as np
 import pandas as pd
-from pdastro import AorB, pdastrostatsclass
-from utils import ColumnNames, Coordinates, CustomLogger
+from pdastro import AnotB, AorB, pdastrostatsclass
+from utils import CleanedColumnNames, Coordinates, CustomLogger, Cut
 
 
 class LightCurve(pdastrostatsclass):
@@ -14,7 +14,7 @@ class LightCurve(pdastrostatsclass):
         pdastrostatsclass.__init__(self)
 
         self.logger = CustomLogger(verbose=verbose)
-        self.colnames = ColumnNames()
+        self.colnames = CleanedColumnNames()
 
         self.control_index = control_index
         self.coords = coords
@@ -22,23 +22,28 @@ class LightCurve(pdastrostatsclass):
     def set_and_preprocess_df(self, t: pd.DataFrame, flux2mag_sigmalimit: float = 3.0):
         self.t = deepcopy(t)
         self._preprocess(flux2mag_sigmalimit=flux2mag_sigmalimit)
-        self._convert_to_skyportal_format()
+        # self._convert_to_skyportal_format()
 
-    def _convert_to_skyportal_format(self):
+    def postprocess(self):
         # convert column names
         if not self.colnames.required.issubset(set(self.t.columns)):
             raise ValueError("Missing expected column")
+        update_cols_dict = {
+            self.colnames.mjd: "mjd",
+            self.colnames.ra: "ra",
+            self.colnames.dec: "dec",
+            self.colnames.mag: "mag",
+            self.colnames.dmag: "magerr",
+            self.colnames.flux: "flux",
+            self.colnames.dflux: "dflux",
+            self.colnames.limiting_mag: "limiting_mag",
+            self.colnames.filter: "filter",
+        }
         self.t.rename(
-            columns={
-                "RA": self.colnames.ra,
-                "Dec": self.colnames.dec,
-                "m": self.colnames.mag,
-                "dm": self.colnames.magerr,
-                "mag5sig": self.colnames.limiting_mag,
-                "F": self.colnames.filter,
-            },
+            columns=update_cols_dict,
             inplace=True,
         )
+        self.colnames.update_many(update_cols_dict)
 
         # drop unnecessary columns
         drop_columns = list(set(self.t.columns.values) - self.colnames.required)
@@ -51,10 +56,14 @@ class LightCurve(pdastrostatsclass):
         self.t.loc[o_ix, self.colnames.filter] = "atlaso"
 
         # add optional columns
+        self.colnames.add_many({"magsys": "magsys", "origin": "origin"})
         self.t[self.colnames.magsys] = "ab"
         self.t[self.colnames.origin] = "fp"
 
     def _preprocess(self, flux2mag_sigmalimit=3.0):
+        # create mask column
+        self.t[self.colnames.mask] = 0
+
         # sort by mjd
         self.t = self.t.sort_values(by=["MJD"], ignore_index=True)
 
@@ -78,16 +87,22 @@ class LightCurve(pdastrostatsclass):
         if "__tmp_SN" in self.t.columns:
             self.t.drop(columns=["__tmp_SN"], inplace=True)
 
+    def get_filt_ix(self, filt: str):
+        return self.ix_equal(self.colnames.filter, filt)
+
     def get_filt_lens(self):
         if self.t is None:
             raise RuntimeError("Table (self.t) cannot be None")
 
         total_len = len(self.t)
         filt_lens = {
-            "o": len(np.where(self.t["F"] == "o")[0]),
-            "c": len(np.where(self.t["F"] == "c")[0]),
+            "o": len(self.get_filt_ix("o")),
+            "c": len(self.get_filt_ix("c")),
         }
         return total_len, filt_lens
+
+    def get_flags(self) -> int:
+        return np.bitwise_or.reduce(self.t[self.colnames.mask])
 
     def get_good_indices(self, flag: Optional[int] = None) -> List[int]:
         # if flag is 0, return all indices
@@ -116,3 +131,45 @@ class LightCurve(pdastrostatsclass):
             # return all masked indices
             flag = self.get_flags()
         return self.ix_masked(self.colnames.mask, maskval=flag)
+
+    def remove_flag(self, flag):
+        self.t[self.colnames.mask] = np.bitwise_and(
+            self.t[self.colnames.mask].astype(int), ~flag
+        )
+
+    def update_mask_column(self, flag, indices, remove_old=True):
+        if remove_old:
+            # remove any old flags of the same value
+            self.remove_flag(flag)
+
+        if len(indices) > 1:
+            flag_arr = np.full(self.t.loc[indices, [self.colnames.mask]].shape, flag)
+            self.t.loc[indices, self.colnames.mask] = np.bitwise_or(
+                self.t.loc[indices, [self.colnames.mask]].astype(int), flag_arr
+            )
+        elif len(indices) == 1:
+            self.t.loc[indices, self.colnames.mask] = (
+                int(self.t.at[indices[0], self.colnames.mask]) | flag
+            )
+
+    def apply_cut(self, cut: Cut, indices: Optional[List[int]] = None):
+        if not cut.can_apply_directly():
+            raise RuntimeError(f"Cannot directly apply the following cut: {cut}")
+        if not cut.column in self.t.columns:
+            raise RuntimeError(
+                f"No column name '{cut.column}' exists in light curve; cannot apply cut"
+            )
+
+        all_ix = self.getindices(indices)
+        kept_ix = self.ix_inrange(
+            colnames=[cut.column],
+            lowlim=cut.min_value,
+            uplim=cut.max_value,
+            indices=all_ix,
+        )
+        cut_ix = AnotB(all_ix, kept_ix)
+
+        self.update_mask_column(cut.flag, cut_ix)
+
+        percent_cut = 100 * len(cut_ix) / len(all_ix)
+        return percent_cut
